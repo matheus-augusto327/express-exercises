dcfc5dfbb06c9cbcb7300085d06049bcbb218ad9
Matheus <matheus.augustoreal@gmail.com>
2022-02-20 10:56:55 -0300

    Learn about static files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
-const PORT = 5000
+app.use(express.static(path.join(__dirname, "client")))
 
+const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`)
 })
