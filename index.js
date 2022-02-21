3744669225b8f45dbe483b1ebeb9b8056a70fe81
Matheus <matheus.augustoreal@gmail.com>
2022-02-21 20:41:14 -0300

    Learn about GET, POST, PUT and DELETE

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const path = require("path")
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "client")))
+app.use("/meusite", express.static(path.join(__dirname, "client")))
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World From GET</h1>")
+})
+
+app.post("/", (req, res) => {
+    res.send("<h1>Hello World From POST</h1>")
+})
+
+app.put("/", (req, res) => {
+    res.send("<h1>Hello World From PUT</h1>")
+})
+
+app.delete("/", (req, res) => {
+    res.send("<h1>Hello World From DELETE</h1>")
+})
 
 const PORT = 5000
+
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`)
 })
