fd501a64599508f5b167898cc9c6f01cb1c0a32b
Matheus <matheus.augustoreal@gmail.com>
2022-02-23 19:53:27 -0300

    Learn about routes

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
-
 const app = express()
 
-app.use("/meusite", express.static(path.join(__dirname, "client")))
+// app.use("/meusite", express.static(path.join(__dirname, "client")))
 
-let consoleBody = (req, res, nesy) => {
-    console.log(req.body)
-    next()
-}
+// let consoleBody = (req, res, nesy) => {
+//     console.log(req.body)
+//     next()
+// } 
 
-let hello = (req, res) => {
-    res.send("Hello World!")
-}
+// let hello = (req, res) => {
+//     res.send("Hello World!")
+// }
 
-app.use("/", bodyParser.json())
-app.use("/", consoleBody)
-app.get("/", hello)
+// app.use("/", bodyParser.json())
+// app.use("/", consoleBody)
+// app.get("/", hello)
 
-app.post("/", hello)
+// app.post("/", hello)
 
-const PORT = 5000
+// const PORT = 5000
 
-app.listen(PORT, () => {
-    console.log(`Server running on Port: ${PORT}`)
+// app.listen(PORT, () => {
+//     console.log(`Server running on Port: ${PORT}`)
+// })
+
+let alunos = [
+    { id: 0, nome: "Jose" },
+    { id: 1, nome: "João" },
+    { id: 2, nome: "Magnocleide" }
+]
+
+app.use(express.urlencoded({extended: false} ))
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
 })
+
+app.get("/alunos", (req, res) => {
+    res.json(JSON.stringify(alunos))
+})
+
+app.get("/aluno", (req, res) => {
+    console.log(req.body)
+    let aluno = alunos[req.body.id]
+    res.json(aluno)
+})
+
+// app.get("/aluno/:id", (req, res) => {
+//     console.log(req.params.body)
+//     let aluno = alunos[req.params.body.id]
+//     res.json(aluno)
+// })
+
+app.listen(3000, () => console.log("Server rodando..."))
